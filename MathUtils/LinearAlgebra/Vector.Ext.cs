7b7549a416bde567e2ac72ce82c8fe978dfb99b8
humgfk <naokiya1129@gmail.com>
2023-03-19 00:20:46 +0900

    Moved statics methods in Vector to Vector.Ext

---
+++ MathUtils/LinearAlgebra/Vector.Ext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUtils.LinearAlgebra
+{
+    public static class VectorExtension
+    {
+        #region Statistics Methods and Properties
+        public static TNum CalcUnbiasedVariance<TNum>(this Vector<TNum> vec) where TNum : IFloatingPointIeee754<TNum>
+        {
+            if (vec.Length == 1) throw new NotSupportedException("Cannnot calculate an unbiased variance for 1 sample.");
+            var mean = vec.Mean;
+            var res = TNum.Zero;
+
+            for (var i = 0; i < vec.Length; i++)
+            {
+                var diff = vec[i] - mean;
+                res += TNum.Pow(diff, TNum.CreateSaturating(2.0));
+            }
+            res /= TNum.CreateSaturating(vec.Length - 1.0);
+
+            return res;
+        }
+        public static TNum CalcStandardDeviation<TNum>(this Vector<TNum> vec) where TNum : IFloatingPointIeee754<TNum>
+        {
+            return TNum.Sqrt(vec.CalcUnbiasedVariance());
+        }
+        public static TNum CalcStandardError<TNum>(this Vector<TNum> vec) where TNum : IFloatingPointIeee754<TNum>
+        {
+            return vec.CalcStandardDeviation() / TNum.Sqrt(TNum.CreateSaturating(vec.Length));
+        }
+        public static TNum CalculateNorm<TNum>(this Vector<TNum> vec) where TNum : IFloatingPointIeee754<TNum>
+        {
+            var res = TNum.Zero;
+            for (var i = 0; i < vec.Length; i++)
+            {
+                res += TNum.Pow(vec[i], TNum.CreateSaturating(2.0));
+            }
+
+            return TNum.Sqrt(res);
+        }
+        #endregion
+    }
+}
